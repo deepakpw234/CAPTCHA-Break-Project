cbe4f58015339eca0bea9d1e624e1efb8397c397
deepakpw234 <deepakpw234@gmail.com>
2024-10-11 15:07:46 +0530

    Deployment

--- application.py
+++ application.py
@@ -36,4 +36,4 @@ def predict_captcha():
         return render_template("home.html",captcha=captcha)
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0",port=8080)
+    app.run(host="0.0.0.0")
