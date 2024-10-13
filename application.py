a9062c851e17459451a90d096b4269eb9a5d0cb7
deepakpw234 <deepakpw234@gmail.com>
2024-10-13 07:14:15 +0530

    Deployment Update v3

--- application.py
+++ application.py
@@ -1,11 +1,11 @@
-from flask import Flask,request,render_template,Response,url_for,redirect
+from flask import Flask,request,render_template,Response
 import pandas as pd
 import numpy as np
 from PIL import Image
 
 from src.pipelines.train_pipeline import TrainPipeline
-from src.pipelines import train_pipeline
 from src.pipelines.predict_pipeline import PredictPipeline
+from src.exception import CustomException
 
 application = Flask(__name__)
 
@@ -17,24 +17,28 @@ def index():
 
 @app.route("/home",methods=["GET","POST"])
 def predict_captcha():
-    if request.method == "GET":
-        return render_template("home.html")
-    else:
-        data = request.files['image']
+    try:
+        if request.method == "GET":
+            return render_template("home.html")
+        
+        else:
+            data = request.files['image']
 
-        data = Image.open(data)
-
-
-        train_pipeline = TrainPipeline()
-        img_crop=train_pipeline.image_spliting(data)
-        df_char = train_pipeline.image_to_binary(img_crop)
+            data = Image.open(data)
 
 
-        predict_pipeline = PredictPipeline()
-        captcha = predict_pipeline.get_prediction(df_char)
+            train_pipe = TrainPipeline()
+            img_crop=train_pipe.image_spliting(data)
+            df_char = train_pipe.image_to_binary(img_crop)
 
-    
-        return render_template("home.html",captcha=captcha)
+
+            predict_pipe = PredictPipeline()
+            captcha = predict_pipe.get_prediction(df_char)
+
+            return render_template("home.html",captcha=captcha)
+        
+    except Exception as e:
+        raise CustomException
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(host="0.0.0.0")
